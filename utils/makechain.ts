f47428cbf653b8f629a7c16647ae8dd44445f799
Gary Kumar <gary.kumar@akqa.com>
2023-05-26 04:19:53 +1000

    Initial commit

---
+++ utils/makechain.ts
@@ -0,0 +1,35 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `
+Create a list of 10 random multiple choice questions with 4 options and hint for each questions on AFL rules.
+Do not include any explanations, only provide a  RFC8259 compliant JSON response  following this format without deviation.
+
+The JSON response:
+`;
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 1, // increase temepreature to get more creative answers
+    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: false, //The number of source documents returned is 4 by default
+    },
+  );
+  return chain;
+};
